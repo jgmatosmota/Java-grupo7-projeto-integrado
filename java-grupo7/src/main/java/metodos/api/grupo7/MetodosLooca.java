7f5912e612b557ea9d12ccfef85a647f4db5bb28
ze <jose.mota@sptech.school>
2023-04-05 20:09:45 -0300

    Projeto inicial com os primeiros métodos

---
+++ java-grupo7/src/main/java/metodos/api/grupo7/MetodosLooca.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package metodos.api.grupo7;
+
+import com.github.britooo.looca.api.core.Looca;
+
+/**
+ *
+ * @author jgmat
+ */
+public class MetodosLooca {
+    public static void main(String[] args) {
+        // após configurar a dependência do Looca no arquivo pom.xml na aba "Project Files"
+        // criamos um objeto "objtLooca" que é composto por vários objetos, sendo cada componente um objeto 
+        Looca objtLooca = new Looca();
+        
+    }
+}
