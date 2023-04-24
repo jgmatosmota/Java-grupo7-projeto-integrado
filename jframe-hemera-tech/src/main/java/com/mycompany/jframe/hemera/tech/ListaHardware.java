74da2fa43c8b56e241f6fc571ba32c5bbba823c0
ze <jose.mota@sptech.school>
2023-04-24 17:58:43 -0300

    Alteração gitignore

--- jframe-hemera-tech/src/main/java/com/mycompany/jframe/hemera/tech/ListaHardware.java
+++ jframe-hemera-tech/src/main/java/com/mycompany/jframe/hemera/tech/ListaHardware.java
@@ -4,6 +4,7 @@
  */
 package com.mycompany.jframe.hemera.tech;
 
+import java.text.DecimalFormat;
 import oshi.hardware.NetworkIF;
 
 /**
@@ -23,57 +24,57 @@ public class ListaHardware {
 //            bytesEnviados = redeInterface.getBytesSent();
 //           
 //        }
-        System.out.println("  _    _                            _______        _     \n" +
-" | |  | |                          |__   __|      | |    \n" +
-" | |__| | ___ _ __ ___   ___ _ __ __ _| | ___  ___| |__  \n" +
-" |  __  |/ _ \\ '_ ` _ \\ / _ \\ '__/ _` | |/ _ \\/ __| '_ \\ \n" +
-" | |  | |  __/ | | | | |  __/ | | (_| | |  __/ (__| | | |\n" +
-" |_|  |_|\\___|_| |_| |_|\\___|_|  \\__,_|_|\\___|\\___|_| |_|\n" +
-"                                                         \n" +
-"                                                         ");
-        System.out.println("-".repeat(30));
-        //Sistema Operacional
+        System.out.println("  _    _                            _______        _     \n"
+                + " | |  | |                          |__   __|      | |    \n"
+                + " | |__| | ___ _ __ ___   ___ _ __ __ _| | ___  ___| |__  \n"
+                + " |  __  |/ _ \\ '_ ` _ \\ / _ \\ '__/ _` | |/ _ \\/ __| '_ \\ \n"
+                + " | |  | |  __/ | | | | |  __/ | | (_| | |  __/ (__| | | |\n"
+                + " |_|  |_|\\___|_| |_| |_|\\___|_|  \\__,_|_|\\___|\\___|_| |_|\n"
+                + "                                                         \n"
+                + "                                                         ");
+        DecimalFormat df = new DecimalFormat("#,###.00");
+//Sistema Operacional
         System.out.println("Sistema Operacional");
         System.out.println(looca.sistemaOperacional);
-        //ProcessadorModelo
+//ProcessadorModelo
         System.out.println("ProcessadorModelo");
         System.out.println(looca.modeloProcessador);
-        //MemoriaModelo
+//MemoriaModelo
         System.out.println("Memoria Ram Total");
-        System.out.println(looca.memoriaTotal);
-        //Não tem modelo!(trouxe total de memória)
-        //info rede
+        System.out.println(df.format((looca.memoriaTotal / (1024.0 * 1024.0))/1000) + " GB");
+//Não tem modelo!(trouxe total de memória)
+//info rede
         System.out.println("info rede");
         System.out.println(looca.hostName);
-        //DiscoModelo
-//        System.out.println(looca.modeloDisco);
-        //Espaço Total Disco
-//        System.out.println(looca.discoTotal);
+//DiscoModelo
+//System.out.println(looca.modeloDisco);
+//Espaço Total Disco
+//System.out.println(looca.discoTotal);
         System.out.println("-".repeat(30));
-        //MemoriaUtilizada(RAM)
+//MemoriaUtilizada(RAM)
         System.out.println("//MemoriaUtilizada(RAM)");
-        System.out.println(looca.memoriaUtilizada);
-        //MemoriaTotal(RAM)
+        System.out.println(df.format((looca.memoriaUtilizada / (1024.0 * 1024.0))/1000) + "GB");
+//MemoriaTotal(RAM)
         System.out.println("MemoriaTotal(RAM)");
-        System.out.println(looca.memoriaTotal);
-        //MemoriaLivre(RAM)
+        System.out.println(df.format((looca.memoriaTotal / (1024.0 * 1024.0)) /1000) + "GB");
+//MemoriaLivre(RAM)
         System.out.println("MemoriaLivre(RAM)");
-        System.out.println(looca.memoriaDisponivel);
-        //DownloadRede
-        System.out.println(redeInterface.getSpeed());
-        System.out.println("DownloadRede");
-        System.out.println(redeInterface.getBytesRecv() / (1024.0 * 1024.0) + " MB/s");
-        //UploadRede
-        System.out.println("UploadRede");
-        System.out.println(redeInterface.getBytesSent() / (1024.0 * 1024.0) + " MB/s");
-        //TemperaturaCPU(talvez)
+        System.out.println(df.format((looca.memoriaDisponivel / (1024.0 * 1024.0)) /1000) + "GB");
+//DownloadRede
+//System.out.println(redeInterface.getSpeed());
+        System.out.println("DownloadRedeTotal");
+        System.out.println(df.format((redeInterface.getBytesRecv() / (1024.0 * 1024.0) /1000)) + "GB");
+//UploadRede
+        System.out.println("UploadRedeTotal");
+        System.out.println(df.format((redeInterface.getBytesSent() / (1024.0 * 1024.0) /1000)) + "GB");
+//TemperaturaCPU(talvez)
         System.out.println("TemperaturaCPU");
         System.out.println(looca.temperaturaAtual);
-        //UsoCPU
+//UsoCPU
         System.out.println("UsoCPU");
         System.out.println(looca.usoCpu);
-        //MemoriaTotal(HD)
+//MemoriaTotal(HD)
         System.out.println("MemoriaTotal(HD)");
-        System.out.println(looca.espacoDisco);
+        System.out.println(df.format((looca.espacoDisco / (1024.0 * 1024.0) /1000)) + "GB");
     }
 }
