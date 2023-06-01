fe732548947f0ec8b0a446db94a3b9c8c732fd7f
ze <jose.mota@sptech.school>
2023-06-01 12:48:44 -0300

    a

--- jframe-hemera-tech/src/main/java/com/mycompany/jframe/hemera/tech/slack/SlackChave.java
+++
@@ -1,8 +0,0 @@
-package com.mycompany.jframe.hemera.tech.slack;
-
-public class SlackChave {
-    private String chave = "https://hooks.slack.com/services/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";
-    public String getChave(){
-        return chave;
-    }
-}
